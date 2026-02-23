a753083d344426cc6d85a68c7455aeaa09b9b86c
Turi Balázs <developer@bali0531.hu>
2026-02-23 15:34:49 +0000

    fix: correct migration 007 down_revision to match 006_add_api_keys

--- plexaddons-api/alembic/versions/007_add_addon_readme_icon.py
+++ plexaddons-api/alembic/versions/007_add_addon_readme_icon.py
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '007'
-down_revision = '006'
+down_revision = '006_add_api_keys'
 branch_labels = None
 depends_on = None
 
